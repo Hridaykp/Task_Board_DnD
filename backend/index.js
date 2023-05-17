9fdef8a04e4a7f0f1889a722427d45b8202b6698
Hriday Kumar Pranit <hridaykp21@gmail.com>
2023-05-17 15:13:58 +0530

    dependencies install

---
+++ backend/index.js
@@ -0,0 +1,41 @@
+import express from "express";
+import mongoose from "mongoose";
+import Env from "dotenv";
+import cors from "cors";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import allRoutes from './routes/index.js'
+Env.config();
+const PORT = process.env.PORT || 8000;
+const app = express();
+
+// middleware 
+app.use(cors());
+app.use(morgan('tiny'));
+app.use(express.json());
+app.use(cookieParser());
+
+//routes
+app.use('/api', allRoutes);
+
+// error handling
+app.use((err, req, res, next) =>{
+    const status = err.statusCode || 500;
+    const message = err.message || "Internal server error";
+    return res.status(status).json({message, stack: err.stack})
+});
+
+// connect database
+const connectDB = async() =>{
+    try{
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log(`Successfuly connected to MongoDB !!`)
+    }catch(err){
+        console.log(err);
+        process.exit(1);
+    }
+}
+app.listen(PORT, ()=>{
+    connectDB();
+    console.log(`Server is running on Port: ${PORT}`)
+})
